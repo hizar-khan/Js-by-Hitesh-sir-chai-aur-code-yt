6db3136683d741056d2d7ea98b9e84978bb231d1
Hizar khan <hizarkhan44546@gmail.com>
2023-10-15 20:26:12 +0500

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,4 +1,23 @@
-alert(2+2) // error coz we are using node 
-let a = 2 
-let b = 3
-console.log(a+b)
+//alert(2+2) // error coz we are using node 
+// there are two type of datatype in js 1_primitive 2_reference
+
+let username = "hziar khan" // string
+let number = '1243' // string
+let num = 343 // integer number
+let java = false //boolean
+let js = true    // boolean
+let nullValue = null // standalone value /object .representation of empty value but null itself null hahhaahah
+let undef = undefined // no value given .assign
+///// we have also some special type like symbol , we will see it  in react mainly use to findout uniquness
+console.log(typeof username)
+console.log(typeof number)
+console.log(typeof java)
+console.log(typeof js)
+console.log(typeof num)
+console.log(typeof nullValue)
+console.log(typeof undef)// undefined type 
+console.log(typeof null) // this will be objects 
+// the above all are primitive data types
+// 2 == Objects
+
+
