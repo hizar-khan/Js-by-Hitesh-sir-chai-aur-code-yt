dd8cd50df9329620d7aae33086c794edcf7e72e4
Hizar Khan <hizarkhan44546@gmail.com>
2023-10-16 21:15:58 +0500

    Operations and some Tricky Opeations

--- 01_basics/04_StringToNumberConfussion.js
+++ 01_basics/04_StringToNumberConfussion.js
@@ -1,3 +1,62 @@
-let a = 3
-let b = 5
-console.log(a+b)
+let someNumer = 343
+ let stringNumber = String(someNumer)
+//  console.log(  stringNumber)
+//  console.log(typeof stringNumber)
+
+ // ******* Operations ***********
+ let value = 6
+ let negValue = -value
+//  console.log( negValue)
+ 
+//  some basic opetations
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**6)
+// console.log(2/6)
+// console.log(2%6)
+
+
+let str1 = "hello"
+let str2 = " hizar"
+//  console.log(`${str1}${str2}`)
+ //or 
+//  console.log(str1 + str2)
+
+// now some confussion and tricky Operations
+ console.log( 1 +  2)
+ console.log('1' +  2)
+ console.log( 1  + "2")
+ console.log("1" + "2") 
+
+ console.log(  1  +  2  + "3") // the first two are numbers so the will be added 1+2 = 3 and the last one is string so final ans will be 33
+ console.log( "1" +  2  + "3")
+ console.log(  1  + "2" +  3  )
+ console.log(  1  + "2" + "3" )
+ console.log( "1"  +  2  + 3)
+ 
+ console.log(true)
+ console.log(+true)// output 1
+ console.log(-true)// output -1
+
+ console.log(false)
+ console.log(+false) // output 0
+ console.log(-false) // -0
+
+ console.log(+"") // also zero
+
+let num1, num2, num3
+num1 = num2 = num3 = 3+3
+console.log(num1)
+console.log(num2)
+console.log(num3)
+
+// let gameScore = 99
+// gameScore++
+// console.log(gameScore) // output 100 due incrment gameScore++
+let gameScore2 = 99
+  ++gameScore2;
+console.log(gameScore2) // same output 100 but remeber that post and prefix incremet are different
+
+
+
