b08c1490d466715a0b6ce08d0001abb3251deb70
Hizar Khan <hizarkhan44546@gmail.com>
2023-10-19 16:01:04 +0500

    summary of data-types

--- 01_basics/05_ComparisionOfDataTypes.js
+++ 01_basics/05_ComparisionOfDataTypes.js
@@ -14,7 +14,7 @@ console.log(a == b)  // output 'true'
 console.log(a === b) //  output 'false'
 // coz here === it means that also check datatype of both value
 // while as a= '2' is string and b = 2 is number so both have different dataType
-//so output is false
+//so output is falseq
 // this called strick check
  
 console.log ('---------null comparison --------')
