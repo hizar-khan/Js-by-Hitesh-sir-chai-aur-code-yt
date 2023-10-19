af4a1f11dd57dc7e2e5dd64b0797ef48d1eb6368
Hizar Khan <hizarkhan44546@gmail.com>
2023-10-19 20:29:16 +0500

    Summary of DataTypes in js

--- 01_basics/06_SummaryOfDataTypes.js
+++ 01_basics/06_SummaryOfDataTypes.js
@@ -7,23 +7,59 @@
 1_Primitive dataTypes the are call by value and change happen in copy 
 they are seven 7
 i_  String 
+let str = "HelloWorld"
+
 ii_ number
+let num = 123
+
 iii_boolean
+let boolTrue = true
+let boolFalse = false
+
 iv_null
+let nullval = null
+
 v_  undefined 
+let und;
+or let undf = undifiend 
+
 vi_ symbol (make values unique)
+let  id = symbol('23')
+
 vii_ BigInt (large values) 
+let bignum = 234325345345235n
 
 */
 
 // 2_ NOn- primitive or reference dataTypes
+// 
 /*
 Arrays
+let arr = ['hizar','khan' , 09]
+
 Objects
-functions()
+let obj = {
+    key      : value,
+    name     : "hizar",
+    lastName : "Khan",
+    rollNo   :  09
+}
+
+functions(){}
+
+let ftnName = (){
+    let name = hizar
+    return name
+}
+ftnName() //calling ftn
 
 */
-// const id = Symbol('123')
-const SndId = Symbol('123')
-console.log(id === SndId);
+
+
+//to find dataType of any value/varible use method "typeof varName"
+// but 
+// to find type of array use  Array.isArray(arrName)
+
+
+
 
