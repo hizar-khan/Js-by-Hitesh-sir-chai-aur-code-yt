0cab8b8dab65e0a90d45f1bbe0a515c3a8f74ab1
Hizar Khan <hizarkhan44546@gmail.com>
2023-10-19 15:56:35 +0500

    Comparison of DataTypes in js

--- 01_basics/05_ComparisionOfDataTypes.js
+++ 01_basics/05_ComparisionOfDataTypes.js
@@ -8,5 +8,19 @@
 
 
 // some confusing Comparison
-
-console.log(2+2)
+let a = '3'
+let b = 3
+console.log(a == b)  // output 'true'
+console.log(a === b) //  output 'false'
+// coz here === it means that also check datatype of both value
+// while as a= '2' is string and b = 2 is number so both have different dataType
+//so output is false
+// this called strick check
+ 
+console.log ('---------null comparison --------')
+console.log(null == 0)     // output 'false'
+console.log(null === 0)   // output 'false'
+console.log(null < 0)    // output 'false'
+console.log(null > 0)   // output 'false'
+console.log(null >= 0) // output 'true'  // here conversion happen the null convert to zero so zero is equal to zero that is why output true . 
+console.log(null <= 0)// output 'true' // here conversion happen the null convert to zero so zero is equal to zero that is why output true . 
