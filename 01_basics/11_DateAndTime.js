f65cb692b0343b5f7b35545d54a195e58901e764
Hizar Khan <hizarkhan44546@gmail.com>
2023-11-21 14:05:16 +0500

    date and time

---
+++ 01_basics/11_DateAndTime.js
@@ -0,0 +1,43 @@
+let myDate = new Date()
+let date = Date()
+
+// console.log(typeof date);
+// console.log(typeof myDate);
+
+// console.log();
+// console.log('-toString()');
+// console.log(myDate.toString());
+// console.log();
+
+// console.log("  --toDateString()-"  );
+// console.log(myDate.toDateString());
+// console.log();
+
+// console.log("  --toLocaleDateString()-"  );
+// console.log(myDate.toLocaleDateString());
+// console.log();
+
+// console.log("  --mtoString()-"  );
+// console.log(myDate.toString());
+// console.log();
+
+// console.log("  --toISOString()-"  );
+// console.log(myDate.toISOString());
+// console.log();
+
+// console.log("  --toUTCString() --" );
+// console.log(myDate.toUTCString());
+
+// console.log("  --toTimeString() --" );
+// console.log(myDate.toTimeString());
+
+// let cstmDate = new Date(2023,0,1)// remeber that month start with 0 = january 1= febuaray
+// console.log(cstmDate.toDateString());
+
+// let cstDate = new Date ("2023-01-02") // but here 01 is january not feb
+// console.log(cstDate.toDateString());
+
+
+// Time -- mostly used in quiz ,polls etc
+let myTimeStamp = Date.now()
+console.log(timeStamp);
