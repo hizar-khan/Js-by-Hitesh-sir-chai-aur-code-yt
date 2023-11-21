54836fe9ad42c6036918b380b4d3c2d47f741bd6
Hizar Khan <hizarkhan44546@gmail.com>
2023-11-21 14:49:54 +0500

    Time and Date in js

--- 01_basics/11_DateAndTime.js
+++ 01_basics/11_DateAndTime.js
@@ -34,7 +34,7 @@
 // let cstmDate = new Date(2023,0,1)// remeber that month start with 0 = january 1= febuaray
 // console.log(cstmDate.toDateString());
 
-let cstDate = new Date ("2023-01-02") // but here 01 is january not feb
+// let cstDate = new Date ("2023-01-02") // but here 01 is january not feb
 // console.log(cstDate.toDateString());
 
 
