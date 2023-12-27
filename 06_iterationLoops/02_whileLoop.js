c87638a491ad8db859afb198bf1b81ecc9fa8c93
Hizar Khan <hizarkhan44546@gmail.com>
2023-12-27 23:13:13 +0500

    while loop in js

---
+++ 06_iterationLoops/02_whileLoop.js
@@ -0,0 +1,45 @@
+// Remember!! Every type of loop have manily three thing i)iniliztiono of variable ii) condition iii) increment/code to terminate the control
+// while loop
+
+// syntx
+/*
+let variable = value;
+ while (condition) { 
+ code to execute 
+ 
+ code to stop the loop/terminate the loop
+ }
+*/
+/**
+ let i = 0;
+while (i <= 10) {
+   
+    console.log(`Value of i is ${i}`);
+   
+    i = i + 2
+
+} 
+   */
+/**
+ let myArr = ['SuperMan', 'batMan', 'SuprrraaMan']
+
+let i = 0
+while (i < myArr.length) {
+    console.log(myArr[i]);
+    i++
+    
+}
+
+ */
+
+// -------------------Do-While loop .here work done first than codition will check mean one time the code will run before cheking condition and loop begining
+/**
+ 
+let i = 1
+do {
+ console.log(`value is : ${i}`);
+  i++
+} 
+while (i > 10);
+
+ */
